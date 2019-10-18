73c029680345350518df4efd45904fe2188ef0d0
deepaksingh1 <deepaksai135@gmail.com>
2019-10-18 18:58:07 +0530

    commits

--- AngApp/src/app/app.module.ts
+++ AngApp/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.components';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule
---
+++ AngApp/src/app/courses.components.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+    // tslint:disable-next-line:component-selector
+    selector: 'courses',
+    template: '<h2>Courses</h2>'
+})
+export class CoursesComponent {
+}
